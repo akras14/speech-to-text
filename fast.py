1b5809cb3b1e3628300985a7c908f1f0037c4389
Alex Kras <akras14@gmail.com>
2018-01-03 23:02:55 -0800

    Working example

---
+++ fast.py
@@ -0,0 +1,47 @@
+import os
+import speech_recognition as sr
+from tqdm import tqdm
+from multiprocessing.dummy import Pool
+pool = Pool(8) # Number of concurrent threads
+
+with open("api-key.json") as f:
+    GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
+
+r = sr.Recognizer()
+files = os.listdir('parts/')
+
+def transcribe(data):
+    idx, file = data
+    name = "parts/" + file
+    print(name + " started")
+    # Load audio file
+    with sr.AudioFile(name) as source:
+        audio = r.record(source)
+    # Transcribe audio file
+    text = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+    print(name + " done")
+    return {
+        "idx": idx,
+        "text": text
+    }
+
+all_text = pool.map(transcribe, enumerate(files))
+pool.close()
+pool.join()
+
+transcript = ""
+for t in sorted(all_text, key=lambda x: x['idx']):
+    total_seconds = t['idx'] * 30
+    # Cool shortcut from:
+    # https://stackoverflow.com/questions/775049/python-time-seconds-to-hms
+    # to get hours, minutes and seconds
+    m, s = divmod(total_seconds, 60)
+    h, m = divmod(m, 60)
+
+    # Format time as h:m:s - 30 seconds of text
+    transcript = transcript + "{:0>2d}:{:0>2d}:{:0>2d} {}\n".format(h, m, s, t['text'])
+
+print(transcript)
+
+with open("transcript.txt", "w") as f:
+    f.write(transcript)
---
+++ slow.py
@@ -0,0 +1,37 @@
+import os
+import speech_recognition as sr
+from tqdm import tqdm
+
+with open("api-key.json") as f:
+    GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
+
+r = sr.Recognizer()
+files = os.listdir('parts/')
+
+all_text = []
+
+for f in tqdm(files):
+    name = "parts/" + f
+    # Load audio file
+    with sr.AudioFile(name) as source:
+        audio = r.record(source)
+    # Transcribe audio file
+    text = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+    all_text.append(text)
+
+transcript = ""
+for i, t in enumerate(all_text):
+    total_seconds = i * 30
+    # Cool shortcut from:
+    # https://stackoverflow.com/questions/775049/python-time-seconds-to-hms
+    # to get hours, minutes and seconds
+    m, s = divmod(total_seconds, 60)
+    h, m = divmod(m, 60)
+
+    # Format time as h:m:s - 30 seconds of text
+    transcript = transcript + "{:0>2d}:{:0>2d}:{:0>2d} {}\n".format(h, m, s, t)
+
+print(transcript)
+
+with open("transcript.txt", "w") as f:
+    f.write(transcript)
