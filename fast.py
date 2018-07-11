2e8a96c19f75862872402d7a959b55f96ef1174a
kfong257 <kfong257@gmail.com>
2018-07-10 20:57:48 -0700

    allow for directory path input updated logging deleted files generated by ffmpeg updated name of file

--- fast.py
+++ fast.py
@@ -4,46 +4,49 @@ import traceback
 import subprocess
 import shlex
 import argparse
+import datetime
+from os.path import isfile, join
 from multiprocessing.dummy import Pool
-pool = Pool(8) # Number of concurrent threads
 
 def do_work(input_file):
 
-    # get metadata
+    pool = Pool(8)  # Number of concurrent threads
     index = input_file.rfind('/')
-    file_dir = input_file[0:index]
     file_name = input_file[index+1:len(input_file)]
+    print("\nTRANSCRIPTION STARTING FOR {0}.".format(file_name.upper()))
 
     # convert input file from .mp3 to .wav
-    # can't do this with ffmpeg because it compresses it in a way that's not suported by wave.py
+    # can't do this with ffmpeg because it compresses it in a way that's not supported by wave.py
     # need to use audacity to convert to wav instead
 
+    #region split .wav file into parts
     file_path = input_file
-    # split .wav file into parts
-    print("START: Splitting {0} into parts.".format(file_path))
-    cmd_in = "ffmpeg -loglevel quiet -i {0} -f segment -segment_time 30 -c copy source/parts/{1}%09d.wav".format(file_path, file_name[:-3])
+    print("Splitting {0} into parts.".format(file_name))
+    cmd_in = "ffmpeg -loglevel quiet -i {0} -f segment -segment_time 30 " \
+             "-c copy source/parts/{1}%09d.wav".format(file_path, file_name[:-3])
     args = shlex.split(cmd_in)
     p = subprocess.Popen(args)
-    print("Waiting for input file to be split")
     p.wait() #necessary in order to avoid starting subsequent work before splitting finishes
-    print("DONE: Split file with ffmpeg.")
+    #endregion
 
     #open api key to google cloud
     with open("api-key.json") as f:
         GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
 
-    # find the relevant files
+    #find only the relevant files
     r = sr.Recognizer()
     file_dir = 'source/parts/'
     all_files = sorted(os.listdir(file_dir))
     files = []
     for i in range(len(all_files)):
         elem = all_files[i]
-        if elem.startswith(file_name[:-4]):
+        if elem.startswith(file_name[:-4]) and file_name[:-4] in elem:
             files.append(elem)
+    print("Split file with ffmpeg into {0} parts.".format(len(files)))
+    #endregion
 
-    # transcribe file parts
-    print("START: Transcribing file parts.")
+    #region transcribe file parts
+    print("Transcribing file parts.")
 
     def transcribe(data):
         idx, file = data
@@ -82,16 +85,34 @@ def do_work(input_file):
         # Format time as h:m:s - 30 seconds of text
         transcript = transcript + "{} ".format(t['text'])
         transcript_csv = transcript_csv + "{:0>2d}:{:0>2d}:{:0>2d}, {}\n".format(h, m, s, t['text'])
+    #endregion
 
-    # print file transcripts
-    with open("transcripts/{0}_transcript.txt".format(file_name[:-4]), "w") as f:
+    #region print file transcripts
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    out1 = "{0}_transcript_{1}.txt".format(file_name[:-4], timestamp)
+    with open("transcripts/" + out1, "w") as f:
         f.write(transcript)
-    print("DONE: Wrote full text to transcript.txt.")
+    print("Wrote full text to {0}.".format(out1))
 
-    with open("transcripts/{0}_transcript.csv".format(file_name[:-4]), "w") as f:
+    out2 = "{0}_transcript_{1}.csv".format(file_name[:-4], timestamp)
+    with open("transcripts/" + out2, "w") as f:
         f.write(transcript_csv)
-    print("DONE: Wrote to transcript.csv with 30s timestamps.")
-    print("DONE: Transcript files are located in {0}/transcripts.".format(os.getcwd()))
+    print("Wrote full text to {0} with 30s timestamps.".format(out2))
+
+    print("Transcript files are located in {0}/transcripts.".format(os.getcwd()))
+    #endregion
+
+    #region clean up
+    # delete file parts
+    for file in files:
+        os.remove(file_dir + file)
+    print("Deleted all file parts.")
+
+    #change source wav file name
+    newname = input_file[:index] + '/' + file_name[:-4] + "_Processed_" + timestamp + ".wav"
+    os.rename(input_file, newname)
+    print("Renamed input file to {0}.".format(newname))
+    #endregion
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -100,4 +121,12 @@ if __name__ == '__main__':
     parser.add_argument(
         'path', help='Full file path to input .wav file.')
     args = parser.parse_args()
-    do_work(args.path)
+    if ".wav" in args.path:
+        do_work(args.path)
+    else:
+        onlyfiles = []
+        for f in os.listdir(args.path):
+            if isfile(join(args.path, f)) and ".wav" in f:
+                onlyfiles.append(os.path.abspath(os.path.join(args.path,f)))
+        for f in onlyfiles:
+            do_work(f)
